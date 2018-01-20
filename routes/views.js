5adc64ca49f114c793e1d586be939d5596fc04a2
Anton-Cao <cao.anton@gmail.com>
2018-01-20 17:29:06 -0500

    Add login functionality

--- routes/views.js
+++ routes/views.js
@@ -7,22 +7,35 @@ router.get('/', (req, res) => {
 
   let allpics = [];
   
-  if (userId) {
-    allpics.push({
-      title: 'Cat',
-      imgSrc: 'http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg'
-    });
-    allpics.push({
-      title: 'Dog',
-      imgSrc: 'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi'
-    });
+  allpics.push({
+    title: 'Cat',
+    imgSrc: 'http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg'
+  });
+  allpics.push({
+    title: 'Dog',
+    imgSrc: 'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi'
+  });
+
+  let loggedIn = false;
+  if (req.session.userId) {
+    loggedIn = true;
   }
    
-  res.render('index', {images: allpics});
+  res.render('index', {images: allpics, loggedIn: loggedIn});
 });
 
 router.get('/signup', (req, res) => {
   res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.get('/logout', function (req, res) {
+  console.log('logging out');
+  req.session.userId = '';
+  res.redirect('/');
 });
 
 module.exports = router;
