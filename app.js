b2adc19c5a6e2e51bb4cc203f7f7b6297ebf926c
sambhav <sambhav.reddy@gmail.com>
2018-01-20 03:30:18 -0500

    fixed minor issues, added GET allpics functionality

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //Stuffs by Sambhav
-
 const express = require("express");
 const bodyParser = require('body-parser')
 const app = express();
@@ -9,7 +8,6 @@ app.use(bodyParser.json())
 //Mongo Setup
 const mongoose = require('mongoose')
 const mongoURL = 'mongodb://dadmin:blueisfour@ds263707.mlab.com:63707/pickit';
-
 mongoose.connect(mongoURL)
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
@@ -19,52 +17,53 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 const imageSchema = new mongoose.Schema({
 	awsKey: String,
 	title: String,
-	ownerId: String
+	ownerId: String 
 });
 const userSchema = new mongoose.Schema({
-	username: String,
-	imageIDs: [String]
-})
-let imgColl = mongoose.model('ImageModel',imageSchema);
-let userColl = mongoose.model('UserModel',userSchema);
+	username: String
+});
+const imgColl = mongoose.model('ImageModel',imageSchema);
+const userColl = mongoose.model('UserModel',userSchema);
 /////////////////
-
-
-
 
 app.post("/newuser",(req,res) => {
 	let username = req.body.username;
 	let newUser = new userColl({
 		'username': username,
-		'imageIDs': []
 	});
 	newUser.save();
-	res.send('User ' + username + ' has been added.')
+	res.send('User ' + username + ' has been added.') 
 });
-
 app.post("/newpic",(req,res,next)=>{
 	let awsKey = req.body.awsKey;
 	let title = req.body.title;
 	let username = req.body.username;
 	userColl.findOne({'username':username}).then(
 	user => {
-		console.log(user)
-		let ownerId = user.username;
-		console.log("OWNERID")
-		console.log(ownerId)
+		let ownerId = user._id;
+		console.log(ownerId);
 		let newPic = new imgColl({
 			'awsKey': awsKey,
 			'title': title,
 			'ownerId': ownerId
 		});
 		newPic.save();
-		user.imageIDs.push(newPic._id);
-		user.save();
-		res.send("New picture, entitled '" + title + "' has been saved.");
+		res.send("New picture, entitled '" + title + "' has been saved."); 
 	},
-	error => {console.log(error);})	
+	error => {console.log(error);})
 });
-
-
-
+app.get("/allpics",(req,res) => {
+	let username = req.query.username;
+	userColl.findOne({'username':username}).then(
+		user => {
+		let userId = user._id;
+		imgColl.find({'ownerId':userId}).then(
+			images => { 
+				let imageKeys = [];
+				images.forEach(image => imageKeys.push(image.awsKey));
+				res.send(imageKeys);
+				}, error => {console.log(error)})
+		},
+		error => {console.log(error)});
+});
 app.listen(3040);
