4c76cabf97651f9f2451dedbbfaa496901025b7d
Sambhav Sunkerneni <sambhav.reddy@gmail.com>
2018-01-20 22:40:46 -0500

    Delete tags.js

--- models/tags.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const tagSchema = new mongoose.Schema({
-  tag: String,
-  picIDs: [String] ,
-  ownerId: String
-});
-
-module.exports = mongoose.model('Tag', tagSchema);
